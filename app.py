97d37a3a34a993331328cb79be5e4e040ee71fc4
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-30 08:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,12 @@
 from fastapi import FastAPI, Request, UploadFile, File
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 import os
-import shutil
-import time
 from utility.Object_Detection import image_processing
 
 app = FastAPI()
 
-# Setup folders
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-# Mount static files (to display images)
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
-# Templates
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.post("/upload", response_class=HTMLResponse)
-async def upload_image(request: Request, file: UploadFile = File(...)):
-    
-    # Unique filename
-    filename = f"{int(time.time())}_{file.filename}"
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
-
-    # Save file
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    # Send image path to HTML
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "filename": filename
-    })
-
